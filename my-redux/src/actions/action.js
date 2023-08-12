de96071bc01a29201e7beffca2c7d284baefb013
akhileshchamoli05 <chamoliakhilesh71@gmail.com>
2023-08-12 21:39:37 +0530

    second commit

--- my-redux/src/actions/action.js
+++ my-redux/src/actions/action.js
@@ -5,9 +5,10 @@ export const incNumber=(num)=>{
     }
 }
 
-export const decNumber=()=>{ 
+export const decNumber=(num)=>{ 
     return{
-        type: "DECREMENT"
+        type: "DECREMENT",
+        payload:num
         
     }
 }
